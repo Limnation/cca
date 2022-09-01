287b94df581d11df37fb37af44877f253babeed9
Scott Erwin <scottallenerwin@gmail.com>
2022-09-01 19:20:49 -0400

    trying a background image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,30 @@
 
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, ImageBackground } from 'react-native';
+
+const image = require('../../assets/pcm.jpg')
 
 const HomePage = () => {
     return (
       <View style={styles.container}>
-        <Image style={styles.header} source={require('../../assets/ccaLogo.png')} />
-        <Image style={styles.welcome} source={require('../../assets/cultureClubAppImage.png')} />
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <Image style={styles.header} source={require('../../assets/ccaLogo.png')} />
+          <Image style={styles.welcome} source={require('../../assets/cultureClubAppImage.png')} />
+        </ImageBackground>
       </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#023047',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#023047',
+      flex: 1,
+      backgroundColor: '#023047',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#023047',
+    },
+      image: {
+      flex: 1,
+      justifyContent: "center"
     },
     header: {
       width: 325,
