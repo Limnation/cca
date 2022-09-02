c43382c5070fe6535c4bd1b91ce92b852186a433
Eduardo Gonzalez <eduardo.gch@gmail.com>
2022-09-02 11:18:43 -0400

    Update site info

--- src/components/SiteInfo.js
+++ src/components/SiteInfo.js
@@ -9,6 +9,14 @@ const SiteInfo = () => {
             <Card.Content>
                 <Title>Atlanta Office</Title>
                 <Paragraph>675 Ponce De Leon Ave NE, Atlanta, GA 30308</Paragraph>
+                <Paragraph>Floor 7th, Suite 7025</Paragraph>
+            </Card.Content>
+        </Card>
+        <Card>
+            <Card.Content>
+                <Title>Parking</Title>
+                <Paragraph>Floor 7th</Paragraph>
+                <Card.Cover source={{ uri: 'https://github.com/Limnation/cca/raw/main/assets/parking.png' }} />
             </Card.Content>
         </Card>
         </View>
